72ab0321c0955d95fccd688997b02d5bfad941e5
Ewa Mitra <ewa.mitra@gmail.com>
2021-02-14 12:29:44 +0100

    html,styles,js

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,36 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+
+const textarea = document.querySelector('.textarea--js');
+const save = document.querySelector('.save--js');
+const load= document.querySelector('.load--js');
+
+save.addEventListener('click',(e)=>{
+    e.preventDefault();
+    localStorage.setItem('entry',textarea.value);
+    if (textarea.value) {
+        document.querySelector(".info--js").innerHTML="ℹ️";
+    }else {
+        document.querySelector(".info--js").innerHTML="";
+    }
+    
+})
+
+
+load.addEventListener('click',(e)=>{
+    e.preventDefault();
+    textarea.value= localStorage.getItem('entry');
+})
+    
+
+
+
+
+
+
+
+
+
+
